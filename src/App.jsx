3e4c61fe1e715f5bd43eb462dfc1abf2bb573c98
Hilder09 <Hilderhernandez1999@gmail.com>
2024-12-19 16:29:11 -0500

    [ADD] Operador logico And

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import OperadorAnd from './components/Navbar/OperadorAnd'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <OperadorAnd/>
 
 
     </>
