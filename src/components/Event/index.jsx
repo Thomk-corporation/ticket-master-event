54b1e693215644cfe9327b93ca26b8c72580cf63
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2024-12-23 22:06:29 -0500

    [REFACTOR] Event component to streamline event rendering and improve search functionality

--- src/components/Event/index.jsx
+++ src/components/Event/index.jsx
@@ -1,47 +1,42 @@
-import { useState } from "react";
-import dataEvents  from "../../data/events.json"
-import EventItem from "./component/EventItem";
+import { useState } from 'react'
+import dataEvents from '../../data/events.json'
+import EventItem from './component/EventItem'
 
+function Event({ searchTerm }) {
+  const [data] = useState(dataEvents)
+  const { events } = data._embedded
 
-function Event({searchTerm}){
-    const [data]= useState(dataEvents)
-    const {events } = data._embedded
+  function handleEventItemClick(id) {
+    console.log(`EventItem con id ${id} clicado`)
+  }
 
-    function handleEventItemClick(id){
-        console.log(`EventItem con id ${id} clicado`)	
+  function renderEvents() {
+    let eventsFiltered = events
 
+    if (searchTerm.length > 0) {
+      eventsFiltered = eventsFiltered.filter((item) =>
+        item.name.toLocaleLowerCase().includes(searchTerm),
+      )
     }
 
-    function renderEvents(){
-        let eventsFiltered = events
+    return eventsFiltered.map((eventItem) => (
+      <EventItem
+        key={`event-item-${eventItem.id}`}
+        name={eventItem.name}
+        image={eventItem.images[1].url}
+        info={eventItem.info}
+        onEventClick={handleEventItemClick}
+        id={eventItem.id}
+      />
+    ))
+  }
 
-
-        if(searchTerm.length>0){
-
-            eventsFiltered = eventsFiltered.filter((item)=>item.name.toLocaleLowerCase().includes(searchTerm))
-
-        }
-
-        return events.map((eventItem) =>(
-            <EventItem
-                key={`event-item-${eventItem.id}`}
-                name = {eventItem.name}
-                image = {eventItem.images[1].url}
-                info = {eventItem.info}
-                onEventClick = {handleEventItemClick}
-                id = {eventItem.id}
-            />     
-        ))
-    }
-
-   
-        return (
-            <div >   
-                Eventos       
-                {renderEvents()} 
-            </div>
-        )
-
+  return (
+    <div>
+      Eventos
+      {renderEvents()}
+    </div>
+  )
 }
 
-export default Event;
+export default Event
