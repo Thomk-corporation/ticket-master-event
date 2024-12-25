5908e7a2ca31f2e027c42a2fbf8a4dd9bab0c2ca
laflopet <laflopet@gmail.com>
2024-12-25 11:24:27 -0500

    [ADD] save to user in json modified

--- src/components/SignupForm/index.jsx
+++ src/components/SignupForm/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 function SignupForm() {
   const [name, setName] = useState('')
@@ -6,6 +7,31 @@ function SignupForm() {
   const [address, setAddress] = useState('')
   const [zipcode, setZipcode] = useState('')
   const [phone, setPhone] = useState('')
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+
+
+
+  function validateForm () {
+    const validateName = /^[a-zA-Z\s]+$/;
+    const validateAge = Number(age);
+    const validateZipcode = /^[0-9]+$/
+    const validatePhone = /^[0-9]{10}$/
+
+    if (!validateName.test(name)) {
+      return 'El nombre solo puede contener letras.'
+    }
+    if (isNaN(validateAge) || validateAge < 0 && validateAge < 150) {
+      return 'La edad debe estar entre 0 y 150'
+    }
+    if (!validateZipcode.test(zipcode)) {
+      return 'El codigo postal solo puede tener numeros'
+    }
+    if (!validatePhone.test(phone)) {
+      return 'El telefono debe contener numeros y debe ser de 10 digitos'
+    }
+    return ""
+  }
 
   function handleClearClick() {
     setName('')
@@ -15,28 +41,47 @@ function SignupForm() {
     setPhone('')
   }
 
-  function handleSubmitForm(event) {
+  async function handleSubmitForm(event) {
     event.preventDefault()
 
-    console.log(
-      'Name:',
+    setError("")
+    setSuccess("")
+
+    const validationError = validateForm();
+
+    if (validationError) {
+      setError(validationError);
+      return;
+    }
+
+    const newUser = {
       name,
-      'Age:',
-      age,
-      'Address:',
+      age: Number(age),
       address,
-      'Zipcode:',
       zipcode,
-      'Phone:',
-      phone,
-    )
+      phone
+    };
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/user', newUser);
+      setSuccess(response.data.message);
+      handleClearClick();
+
+    } catch (error) {
+      setError(error.response.data.message)
+        console.log(error)
+    }
+
   }
 
   return (
     <form onSubmit={handleSubmitForm}>
+      {success && <div style={{ color: 'green' }}>{success}</div>}
+      {error && <div style={{ color: 'red' }}>{error}</div>}
       <label>
-        Name
+        Name: 
         <input
+          type='text'
           value={name}
           onChange={(event) => setName(event.target.value)}
           required
@@ -44,8 +89,9 @@ function SignupForm() {
       </label>
       <br />
       <label>
-        Age
+        Age: 
         <input
+          type='number'
           value={age}
           onChange={(event) => setAge(event.target.value)}
           required
@@ -53,7 +99,7 @@ function SignupForm() {
       </label>
       <br />
       <label>
-        Address
+        Address: 
         <input
           value={address}
           onChange={(event) => setAddress(event.target.value)}
@@ -62,8 +108,9 @@ function SignupForm() {
       </label>
       <br />
       <label>
-        Zipcode
+        Zipcode: 
         <input
+          type='text'
           value={zipcode}
           onChange={(event) => setZipcode(event.target.value)}
           required
@@ -71,8 +118,9 @@ function SignupForm() {
       </label>
       <br />
       <label>
-        Phone
+        Phone: 
         <input
+          type='text'
           value={phone}
           onChange={(event) => setPhone(event.target.value)}
           required
